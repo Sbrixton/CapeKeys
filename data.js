46499432806b59de8378fc1415737af7cb7a0ca1
Sbrixton <sanelebangani7@gmail.com>
2025-05-31 20:07:30 +0200

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ if (!localStorage.getItem('products')) {
         name: "Aston Martin",
         price: 6400,
         image: "Aston martin (1).jpeg",
-        page: "Sbrixton.github.io/CapeKeys/Aston.html"
+        page: "Aston.html"
       },
       {
         name: "BMW 4 Series 420d Coupe M Sport ",
